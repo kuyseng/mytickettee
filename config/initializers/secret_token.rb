cf9b8f252cf3bd8eb4d71ccd73d0d2feb1ae1224
kuyseng <kuyseng@gmail.com>
2013-07-07 21:38:51 +0700

    first setup rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myticketee::Application.config.secret_token = 'f99d21ad3089dbfc65e3cbbe71695dfdb5f0c796703ae5c63a8f69ccbe5c608e56958bcdbbad30efe24c29ff15892b256a7648c821d7c8b409fe0b33cd81da1d'
